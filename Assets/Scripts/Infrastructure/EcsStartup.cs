9b911bc644d032f2da4fc5ade3d87c2c36fd46b5
Vitaliy-Todorov <65270620+Vitaliy-Todorov@users.noreply.github.com>
2022-09-16 13:03:58 +0300

    EcsStartup

---
+++ Assets/Scripts/Infrastructure/EcsStartup.cs
@@ -0,0 +1,40 @@
+using Leopotam.Ecs;
+using UnityEngine;
+
+namespace Assets.Scripts.Infrastructure
+{
+    public class EcsStartup : MonoBehaviour
+    {
+        private EcsWorld _world;
+        private EcsSystems _systems;
+
+        void Start()
+        {
+            _world = new EcsWorld();
+            _systems = new EcsSystems(_world);
+
+#if UNITY_EDITOR
+            Leopotam.Ecs.UnityIntegration.EcsWorldObserver.Create(_world);
+            Leopotam.Ecs.UnityIntegration.EcsSystemsObserver.Create(_systems);
+#endif
+
+            _systems.Init();
+        }
+
+        void Update()
+        {
+            _systems?.Run();
+        }
+
+        private void OnDestroy()
+        {
+            if (_systems != null)
+            {
+                _systems.Destroy();
+                _systems = null;
+                _world.Destroy();
+                _world = null;
+            }
+        }
+    }
+}
